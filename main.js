131b865c2fb2e322b7e70ab3dc200f3dfd77b528
Himanshu3790 <95909610+Himanshu3790@users.noreply.github.com>
2022-05-18 06:47:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var player1_name= "";
+var player2_name= "";
+
+function adduser(){
+player1_name=document.getElementById("player_1_name").value;
+player2_name=document.getElementById("player_2_name").value;
+}
